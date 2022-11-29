a861f6af5988863d9415c5b9577b89e28d930494
Aval29 <Ahdereavalon@yandex.ru>
2022-11-29 07:12:21 +0300

    add  scale Progres прототип

--- Assets/Scripts/Scene_2/ClickFireplace.cs
+++ Assets/Scripts/Scene_2/ClickFireplace.cs
@@ -1,29 +1,80 @@
 
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.UI;
 
 public class ClickFireplace : MonoBehaviour, IPointerClickHandler
 {
-    //
+    /// <summary>
+    /// Модель обьекта по уровням от 0 -сломано до n- максимум
+    /// </summary>
     [SerializeField]
-    private GameObject[] _аctivate;
+    private GameObject[] _objectModel;
+    /// <summary>
+    /// Модель обьекта сейчас
+    /// </summary>
+    [SerializeField]
+    private GameObject _objectNow;
+    /// <summary>
+    /// Уровень обьекта 
+    /// </summary>
+[SerializeField] 
+    private int _lvObject = 0;
+    /// <summary>
+    /// Максимальный Уровень обьекта 
+    /// </summary>
+[SerializeField]
+    private int _lvObjectMax;
+    /// <summary>
+    ///      Шкала для ремонта фоновая
+    /// </summary>
+    [SerializeField]
+    private GameObject _scaleProgress;
+    /// <summary>
+    ///     Шкала для ремонта заполнения (прогресса)
+    /// </summary>
+    [SerializeField]
+    private GameObject _scaleProgressUp;
+    /// <summary>
+    /// Колличество необходимого ресурса
+    /// </summary>
+    [SerializeField]
+    private int _amtRequiredResourceGoLvUp = 1;
+    /// <summary>
+    /// Добавленно ресурса для Up
+    /// </summary>
+[SerializeField]
+    private int _amtAddResource = 0;
+
+
+
+    private void Start()
+    {
+        _lvObjectMax = _objectModel.Length -1;
+        AddModel(_lvObject);
+    }
 
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-        if (_аctivate[0].activeSelf)
+
+        // проверка ресурса
+         if (_lvObject < _lvObjectMax  ) //&& _amtRequiredResource >= ) в инвентаре
         {
-            for (int i = 0; i < _аctivate.Length; i++)
+            _amtAddResource += 1;
+            ScaleProgress(true);
+            if (_amtRequiredResourceGoLvUp == _amtAddResource)
             {
-                _аctivate[i].SetActive(false);
+                LvUp();
+                _amtAddResource = 0;
+                ScaleProgress(false);
             }
-        }
+
+
+
+        } // русурса не хватает
         else
         {
-            for (int i = 0; i < _аctivate.Length; i++)
-            {
-                _аctivate[i].SetActive(true);
-            }
+            
         }
 
 
@@ -31,7 +82,52 @@ public class ClickFireplace : MonoBehaviour, IPointerClickHandler
     }
 
 
+    /// <summary>
+    /// повышение уронвя
+    /// </summary>
+   private void LvUp()
+    {
+        _lvObject += 1;
+         AddModel(_lvObject);
+    }
 
 
+    /// <summary>
+    /// Замена обьекта выше уровнем
+    /// </summary>
+    /// <param name="_LvMod"></param>
+    private void AddModel(int _LvMod)
+    {
+        Destroy(_objectNow);
+        if (_LvMod <= _objectModel.Length && _LvMod >= 0)
+        {
+            _objectNow = Instantiate(_objectModel[_LvMod], transform.position, Quaternion.Euler(0f, 140f, 0f));
+            _objectNow.transform.localScale = new Vector3(3f, 3f, 3f);
+            _objectNow.transform.SetParent(transform);
+            _objectNow.transform.SetAsFirstSibling(); // Ввеерх списка
+        }
+    }
+
+
+    /// <summary>
+    /// Шкала прогресса
+    /// </summary>
+    /// <param name="OnOff">true= вкл,false=выкл</param>
+    private void ScaleProgress (bool OnOff)
+    {
+        if (OnOff == true)
+        {
+            _scaleProgress.SetActive(true);
+            _scaleProgressUp.SetActive(true);
+            float _progressResource = (float)_amtAddResource / (float)_amtRequiredResourceGoLvUp;
+            { _scaleProgressUp.GetComponent<Image>().fillAmount = _progressResource; }
+        }
+        else
+        {
+            _scaleProgress.SetActive(false);
+            _scaleProgressUp.SetActive(false);
+        }
+
+    }
 
 }
