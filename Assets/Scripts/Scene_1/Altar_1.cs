1d5e92d86bdda6ec8b6e97d840c8208d2cdf2bd3
Vladimir Vladimirovich <58000132+Vladimir-MaSTeR@users.noreply.github.com>
2023-04-12 17:16:13 +0700

    изменение доски

--- Assets/Scripts/Scene_1/Altar_1.cs
+++ Assets/Scripts/Scene_1/Altar_1.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Altar_1 : MonoBehaviour, IDropHandler {
+public class Altar_1 : MonoBehaviour, IDropHandler, IPointerDownHandler {
 
     #region Переменные движка
     [Header("Настройки")]
@@ -41,14 +41,14 @@ public class Altar_1 : MonoBehaviour, IDropHandler {
         _currentTime = 0;
 
         //_completeImage.gameObject.SetActive(false);
-        _timerTextUI.gameObject.SetActive(true);
+        //_timerTextUI.gameObject.SetActive(true);
 
         ReloadSaveTimer();
     }
 
-    private void Update() {
-        Timer();
-    }
+    //private void Update() {
+    //    Timer();
+    //}
 
     public void OnDrop(PointerEventData eventData) {
         if(ResourcesTags.Spark.ToString() == eventData.pointerDrag.tag) {
@@ -68,41 +68,64 @@ public class Altar_1 : MonoBehaviour, IDropHandler {
         }
     }
 
-    private void OnEnable() {
+    public void OnPointerDown(PointerEventData eventData) {
+
+        var currentSparcs = EventsResources.onGetSparkCurrentValue?.Invoke();
+
+        if(currentSparcs > 0) {
+
+            //Возвращаем искорку на своё место
+            //eventData.pointerDrag.transform.localPosition = Vector3.zero;
+            //eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+            //вызываем эвент мгновенно сдвига вниз рун на одну строчку и обнуляе счетчик до следующего “сдвига” до изначального
+            MeargGameEvents.onSetTimeToSpawnRuns?.Invoke(0);
+
+            //вызывать эвент уменьшения искорки.
+            EventsResources.onAddOrDeductSparkValue?.Invoke(1, false);
+
+
+            // Debug.Log($"Искру перетащили на алтарь, нужно убавить время");
+        } else {
+            //Проигрывать звук отказа использования алтаря 
+        }
+    }
+
+private void OnEnable() {
         ButtonsEvents.onSaveResouces += SaveTimer;
-        MeargGameEvents.onSetTimeToSpawnRuns += UpdateCurrentTime;
+        //MeargGameEvents.onSetTimeToSpawnRuns += UpdateCurrentTime;
     }
 
     private void OnDisable() {
         ButtonsEvents.onSaveResouces -= SaveTimer;
-        MeargGameEvents.onSetTimeToSpawnRuns -= UpdateCurrentTime;
+        //MeargGameEvents.onSetTimeToSpawnRuns -= UpdateCurrentTime;
     }
 
-    private void UpdateCurrentTime(float time) {
-        _currentTime = time;
-    }
+    //private void UpdateCurrentTime(float time) {
+    //    _currentTime = time;
+    //}
 
-    private void Timer() {
-        if(_currentTime > 0) {
-            _currentTime -= Time.deltaTime;
-            UpdateTimerText(_currentTime);
+    //private void Timer() {
+    //    if(_currentTime > 0) {
+    //        _currentTime -= Time.deltaTime;
+    //        UpdateTimerText(_currentTime);
 
-        } else {
-            _currentTime = (float)(MeargGameEvents.onGetCurrentTimeSpawnOldColumn?.Invoke());
-            //_completeImage.gameObject.SetActive(true);
-        }
-    }
+    //    } else {
+    //        _currentTime = (float)(MeargGameEvents.onGetCurrentTimeSpawnOldColumn?.Invoke());
+    //        //_completeImage.gameObject.SetActive(true);
+    //    }
+    //}
 
-    private void UpdateTimerText(float time) {
-        if(time < 0) {
-            time = 0;
-        }
+    //private void UpdateTimerText(float time) {
+    //    if(time < 0) {
+    //        time = 0;
+    //    }
 
-        var minutes = Mathf.FloorToInt(time / 60);
-        var seconds = Mathf.FloorToInt(time % 60);
-        _timerTextUI.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    //    var minutes = Mathf.FloorToInt(time / 60);
+    //    var seconds = Mathf.FloorToInt(time % 60);
+    //    _timerTextUI.text = string.Format("{0:00} : {1:00}", minutes, seconds);
 
-    }
+    //}
 
     private void SaveTimer() {
         PlayerPrefs.SetFloat(timeName, _currentTime);
