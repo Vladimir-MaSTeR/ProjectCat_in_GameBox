ee3271219bceb8599c7459ffd9790f673e3dd97d
Vladimir Vladimirovich <58000132+Vladimir-MaSTeR@users.noreply.github.com>
2023-04-25 17:11:54 +0700

    Добавил анимации все видам рун.

--- Assets/Scripts/Scene_1/EndAnim.cs
+++ Assets/Scripts/Scene_1/EndAnim.cs
@@ -1,10 +1,8 @@
 using System;
 using UnityEngine;
 public class EndAnim : MonoBehaviour {
-
-
     public void EndAnimation() {
-        Debug.Log("По Идее должна уничтожится руна после анаимации");
+        // Debug.Log("По Идее должна уничтожится руна после анаимации");
         Destroy(this.gameObject.GetComponentInParent<Item_3d>().gameObject);
         MeargGameEvents.onSetStartTimerTrue?.Invoke();
     }
